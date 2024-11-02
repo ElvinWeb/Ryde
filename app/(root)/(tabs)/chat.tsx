fa561e3bd8d2554d0b4c421a355f976bd80a28af
Elvin <elvinsrkrov@gmail.com>
2024-11-02 20:41:40 +0400

    initial project setup && folder structure has created

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+import React from "react";
+
+const Chat = () => {
+  return (
+    <SafeAreaView>
+      <Text>chat</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Chat;
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+import React from "react";
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>rides</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
