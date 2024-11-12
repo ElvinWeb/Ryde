1f3a9e87f4048c08632bea0c98063fabd6222ec9
Elvin <elvinsrkrov@gmail.com>
2024-11-12 17:15:34 +0400

    Onboarding screens have implemented

---
+++ components/CustomButton.tsx
@@ -0,0 +1,33 @@
+import { Text, TouchableOpacity } from "react-native";
+import { getBgVariantStyle, getTextVariantStyle } from "@/lib/utils";
+import { ButtonProps } from "@/types/type";
+
+
+const CustomButton = ({
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  IconLeft,
+  IconRight,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className={`w-full rounded-full p-3 flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(
+        bgVariant
+      )} ${className}`}
+      {...props}
+    >
+      {IconLeft && <IconLeft />}
+      <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+        {title}
+      </Text>
+      {IconRight && <IconRight />}
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
